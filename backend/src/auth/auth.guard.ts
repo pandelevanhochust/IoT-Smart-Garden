cde7dec6316488c8a36810161c5adc092df737df
Lê Văn Toàn <Toan.LV225160@sis.hust.edu.vn>
2025-08-19 18:07:05 +0700

    Add files via upload

---
+++ backend/src/auth/auth.guard.ts
@@ -0,0 +1,42 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { CanActivate, ExecutionContext } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { Request } from 'express';
+import { jwtConst } from '../constant';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  constructor(private readonly jwtService: JwtService) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest<Request>();
+    const token = this.extractTokenFromHeader(request);
+
+    if (!token) {
+      throw new UnauthorizedException('Missing token');
+    }
+
+    try {
+      const payload = await this.jwtService.verifyAsync(token, {
+        secret: jwtConst,
+      });
+
+    request['user'] = payload;
+    console.log("Here\n",request["user"]);            
+    } catch (error) {
+      console.log("Reach invalid token\n");
+      console.error('JWT verification failed:', error);
+      throw new UnauthorizedException(error.message);
+    }
+    
+    return true;
+  }
+
+  private extractTokenFromHeader(request: Request): string | undefined {
+    const authHeader = request.headers.authorization;
+    if (!authHeader) return undefined;
+
+    const [type, token] = authHeader.split(' ');
+    return type === 'Bearer' ? token : undefined;
+  }
+}
