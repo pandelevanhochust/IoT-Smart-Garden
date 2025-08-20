a6cd07dea763889a0aafef0a3bb25744a58f29d4
Lê Văn Toàn <Toan.LV225160@sis.hust.edu.vn>
2025-08-20 17:55:33 +0700

    Update main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,10 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
+import { JwtAuthGuard } from './auth/passport/jwt-strategy/jwt-auth.guard';
+import { AuthGuard } from '@nestjs/passport';
+import { AuthModule } from './auth/auth.module';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.set('query parser', 'extended');  
+  
+
   if (await app.listen(process.env.PORT ?? 3000)) console.log("Server starting");
   console.log(app.getHttpServer().address());
 }
