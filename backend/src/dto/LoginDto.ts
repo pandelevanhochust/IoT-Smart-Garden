c3856ef8280bfd477e66b21b721e997ff1ef1f78
Lê Văn Toàn <Toan.LV225160@sis.hust.edu.vn>
2025-08-22 17:55:34 +0700

    Update LoginDto.ts

--- backend/src/dto/LoginDto.ts
+++ backend/src/dto/LoginDto.ts
@@ -1,8 +1,12 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsString } from 'class-validator';
 
 export class LoginDto {
     @IsString()
+    @ApiProperty({ example: 'tensou' })
     username: string;
+
     @IsString()
+    @ApiProperty({ example: 'thispass' })
     password: string;
     }
