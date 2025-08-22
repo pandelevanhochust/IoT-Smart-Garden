9528515fac938b8fea7dd1f46a7c4f4b800cabfb
Lê Văn Toàn <Toan.LV225160@sis.hust.edu.vn>
2025-08-22 17:55:48 +0700

    Update post.dto.ts

--- backend/src/dto/post.dto.ts
+++ backend/src/dto/post.dto.ts
@@ -1,10 +1,13 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsInt, IsString } from 'class-validator';
 
 export class PostDto {
   @IsString()
+  @ApiProperty({ example: 'Getting Started with NestJS' })
   title: string;
 
   @IsString()
+  @ApiProperty({ example: 'NestJS is a progressive Node.js framework for building efficient and scalable server-side applications.' })
   content: string;
 
   @IsInt()
