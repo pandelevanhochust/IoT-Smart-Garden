998d74cef489af8c3fc405740f40b3d1d44f56fc
Lê Văn Toàn <Toan.LV225160@sis.hust.edu.vn>
2025-08-22 17:56:28 +0700

    Update auth.controller.ts

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,8 +1,10 @@
 import { Body, Controller, HttpCode, HttpStatus, Post, Request, UseGuards } from '@nestjs/common';
 import { RegisterDto } from '../dto/RegisterDto';
 import { AuthService } from './auth.service';
-import { Public } from './guard/roles.decorator';
+import { Public, PUBLIC_KEY } from 'src/decorator/public.decorator';
 import { LocalAuthGuard } from './passport/local-strategy/local-auth.guard';
+import { ApiBody } from '@nestjs/swagger';
+import { LoginDto } from 'src/dto/LoginDto';
 
 @Controller('auth')
 export class AuthController {
@@ -19,6 +21,7 @@ export class AuthController {
   @UseGuards(LocalAuthGuard)
   @HttpCode(HttpStatus.OK)
   @Post('login')
+  @ApiBody({ type: LoginDto })
   async logIn(@Request() request): Promise<object> {
     console.log("In controller\n",request.user);
     return this.authService.login(request.user.username,request.user.password);
@@ -27,6 +30,7 @@ export class AuthController {
   @Public()
   @HttpCode(HttpStatus.OK)
   @Post('register')
+  @ApiBody({ type: RegisterDto })
   async register(@Body() userData: RegisterDto): Promise<object> {
     return this.authService.register(userData);
   }
