ea33df4360fe38620e9580359a2033306724b0b5
Lê Văn Toàn <Toan.LV225160@sis.hust.edu.vn>
2025-08-22 17:55:00 +0700

    Update logger.middleware.ts

--- backend/src/middleware/logger.middleware.ts
+++ backend/src/middleware/logger.middleware.ts
@@ -1,9 +1,16 @@
 import { Injectable, NestMiddleware } from '@nestjs/common';
-import { NextFunction } from 'express';
+import { Request, Response, NextFunction } from 'express';
 
 @Injectable()
 export class LoggerMiddleware implements NestMiddleware {
     use(req: Request, res: Response, next: NextFunction){
+        const {method, params, query, url } = req;
+        const start = Date.now();
+
+        res.on('finish',() => {              
+            const duration = Date.now() - start;
+            console.log(`[${method}] - ${url} - ${res.statusCode} - (${duration}ms)`);
+        })
         console.log("Hello");
         next();
     }
