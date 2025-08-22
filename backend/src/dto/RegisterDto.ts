0b7a4669a2081265270a9476d0e8a9182cc47d1b
Lê Văn Toàn <Toan.LV225160@sis.hust.edu.vn>
2025-08-22 17:56:01 +0700

    Update RegisterDto.ts

--- backend/src/dto/RegisterDto.ts
+++ backend/src/dto/RegisterDto.ts
@@ -1,16 +1,21 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsOptional, IsString } from 'class-validator';
 
 export class RegisterDto {
   @IsString()
+  @ApiProperty({ example: 'Johny Le' })
   name: string;
 
   @IsString()
+  @ApiProperty({ example: 'gosei' })
   username: string;
 
   @IsEmail()
+  @ApiProperty({ example: 'takeru@example.com' })
   email: string;
 
   @IsString()
+  @ApiProperty({ example: 'thispass' })
   password: string;
 
   @IsOptional()
@@ -18,13 +23,16 @@ export class RegisterDto {
   hashedPassword: string;
 
   @IsString()
+  @ApiProperty({ example: 'admin' })
   role: string;
 
   @IsOptional()
   @IsString()
+  @ApiProperty({ example: 'Fullstack engineer from Hanoi' })
   bio?: string;
 
   @IsOptional()
   @IsString()
+  @ApiProperty({ example: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAA...' })
   avatar?: string;
 }
