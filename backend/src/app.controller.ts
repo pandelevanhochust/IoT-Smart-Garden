307b1468bd20a26de6bcd35405dd59336fea7ece
Lê Văn Toàn <Toan.LV225160@sis.hust.edu.vn>
2025-08-22 17:53:35 +0700

    Update app.controller.ts

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,12 +1,16 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Request } from '@nestjs/common';
+import { ApiBearerAuth } from '@nestjs/swagger';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
+  @ApiBearerAuth('JWT-auth')
+  getHello(@Request() request): string {
+    console.log(request);
     return this.appService.getHello();
   }
 }
